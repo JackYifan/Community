0fd028f04e9e240746e1bfd0b74c0b29200be203
Jackwyf2019 <1779626055@qq.com>
2022-04-15 23:12:47 +0800

    增加个人信息页面

---
+++ src/main/java/com/isee/community/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.isee.community.controller;
+
+import com.isee.community.dto.FileDTO;
+import com.isee.community.model.User;
+import com.isee.community.service.FileService;
+import com.isee.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author Yifan Wu
+ * Date on 2022/4/15  21:47
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping("/userinfo")
+    public String userInfo(Model model,HttpServletRequest request){
+        //如果没有登录跳转到登录页面获得当前用户信息
+        User user = (User) request.getSession().getAttribute("user");
+        if(user==null) return "redirect:/";
+
+        model.addAttribute("user",user);
+        return "userinfo";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateInfo(@RequestParam(value = "avatar") MultipartFile file,
+                             String username, String bio, Model model, HttpServletRequest request){
+        FileDTO fileDTO = fileService.upload(file);
+        User user = (User) request.getSession().getAttribute("user");
+        user.setAvatarUrl(fileDTO.getUrl());
+        user.setName(username);
+        user.setBio(bio);
+        userService.updateById(user);
+        model.addAttribute("user",user);
+        return "userinfo";
+    }
+}
---
+++ src/main/java/com/isee/community/service/FileService.java
@@ -0,0 +1,52 @@
+package com.isee.community.service;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.isee.community.dto.FileDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.IOException;
+
+/**
+ * @Author Yifan Wu
+ * Date on 2022/4/15  22:49
+ */
+
+
+
+@Service
+public class FileService {
+    @Value("${aliyun.oss.file.endpoint}")
+    String endpoint ;
+    @Value("${aliyun.oss.file.keyid}")
+    String accessKeyId ;
+    @Value("${aliyun.oss.file.keysecret}")
+    String accessKeySecret ;
+    @Value("${aliyun.oss.file.bucketname}")
+    String bucketName ;
+
+    public FileDTO upload(MultipartFile file){
+        String fileName=file.getOriginalFilename();
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        try {
+            ossClient.putObject(bucketName, fileName, file.getInputStream());
+            ossClient.shutdown();
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            String utl = "https://"+bucketName+"."+endpoint+"/"+fileName;
+            fileDTO.setUrl(utl);
+            return fileDTO;
+        } catch (IOException e) {
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return fileDTO;
+        }
+
+
+
+    }
+}
