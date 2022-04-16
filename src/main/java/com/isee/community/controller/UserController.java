591bced5bb169d3af3884b2e064ab9cf821dec27
Jackwyf2019 <1779626055@qq.com>
2022-04-16 13:34:17 +0800

    增加点击头像查看用户功能

--- src/main/java/com/isee/community/controller/UserController.java
+++ src/main/java/com/isee/community/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -50,5 +51,14 @@ public class UserController {
         userService.updateById(user);
         model.addAttribute("user",user);
         return "userinfo";
+    }
+
+    @RequestMapping("/user/{userId}")
+    public String userIntro(@PathVariable(name = "userId")Long id,
+                                   Model model){
+        User user = userService.getById(id);
+        model.addAttribute("user",user);
+        return "userintro";
+
     }
 }
