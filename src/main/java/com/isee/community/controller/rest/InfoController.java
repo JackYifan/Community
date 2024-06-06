460ce91652d4bd53a5a3b3d86e14e32c244b839d
JackYifan <1779626055@qq.com>
2024-06-06 21:04:47 +0800

    feat: add question rest api

--- src/main/java/com/isee/community/controller/rest/InfoController.java
+++ src/main/java/com/isee/community/controller/rest/InfoController.java
@@ -7,34 +7,29 @@ import com.isee.community.dto.QuestionDTO;
 import com.isee.community.model.Question;
 import com.isee.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api/question")
 public class InfoController {
 
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/getQuestions")
-    public PaginationDTO getQuestions(
-            @RequestParam(name = "page", defaultValue = "1") Integer page,
-            @RequestParam(name = "size", defaultValue = "5") Integer size,
-            @RequestParam(name = "search", required = false) String search) {
 
-        //根据request中的cookie信息查询数据库,以通过拦截器完成
-        PaginationDTO pagination = questionService.list(search, page, size);
-        return pagination;
-    }
-
-    @GetMapping("/question/list")
+    @RequestMapping("/list")
     public IPage<QuestionDTO> listQuestions(
             @RequestParam(name = "page", defaultValue = "1") Integer page,
             @RequestParam(name = "size", defaultValue = "5") Integer size) {
         IPage<QuestionDTO> questionDTOIPage = questionService.list(page, size);
         return questionDTOIPage;
     }
+    @RequestMapping("/{id}")
+    public QuestionDTO info(@PathVariable("id") Long id) {
+        return questionService.getById(id);
+    }
+
+
 }
